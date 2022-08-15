7622ccb2430b1454665557adb79f52aec1983c6d
urvashi-19 <0308urvashi@gmail.com>
2022-08-15 20:57:12 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom"
 import {BrowserRouter as Router , Routes , Route , Link} from "react-router-dom";
 import './index.css';
 import App from './App';
-import Home from "./routes/Home";
+// import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contacts from "./routes/Contacts";
@@ -16,13 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-  <BrowserRouter>
+  <BrowserRouter browser={process.env.PUBLIC_URL}>
   <Routes>
-  <Route path ='/' element = {<App/>}/>
-  <Route path ='/' element = {<Home/>}/>
-  <Route path ='/about' element = {<About/>}/>
-  <Route path ='/contacts' element = {<Contacts/>}/>
-  <Route path ='/project' element = {<Project/>}/>
+  <Route exact path ='/' element = {<App/>}/>
+  {/* <Route path ='/' element = {<Home/>}/> */}
+  <Route exact path ='/about' element = {<About/>}/>
+  <Route  exact path ='/contacts' element = {<Contacts/>}/>
+  <Route  exact path ='/project' element = {<Project/>}/>
 
   
   
